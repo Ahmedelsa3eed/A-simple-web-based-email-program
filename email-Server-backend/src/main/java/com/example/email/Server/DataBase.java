5e4d06afd10ca2ed9fc0dd1f5b6696ea333d71af
AhmedElemary57 <honor5766@gmail.com>
2022-09-14 20:33:09 +0200

    MongoDB for Register

--- email-Server-backend/src/main/java/com/example/email/Server/DataBase.java
+++ email-Server-backend/src/main/java/com/example/email/Server/DataBase.java
@@ -7,19 +7,17 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import org.json.simple.JSONObject;
 
 public class DataBase {
 
     private static DataBase instance = null;
-    private MongoClient mongoClient;
-    private MongoDatabase database;
+    private final MongoDatabase database;
     private DataBase(){
         ConnectionString connectionString = new ConnectionString("mongodb+srv://MailServer:Mailserver123@mailserver.5krhxva.mongodb.net/?retryWrites=true&w=majority");
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
-        this.mongoClient = MongoClients.create(settings);
+        MongoClient mongoClient = MongoClients.create(settings);
         this.database = mongoClient.getDatabase("MailServer");
 
     }
@@ -31,36 +29,28 @@ public class DataBase {
         return instance;
 
     }
-
-    public MongoDatabase getDatabase() {
-        return database;
-    }
     public User getUser(String email){
         Document document = database.getCollection("Users").find(new Document("email", email)).first();
-        JSONObject jsonObject = new JSONObject();
-        User user = new User();
-        user.setFirstName((String) document.get("firstName"));
-        user.setSecondName((String) document.get("secondName"));
-        user.setEmail((String) document.get("email"));
-        user.setPassword((String) document.get("password"));
+        if(document != null){
 
-        return user;
-    }
-    public String getPassword(String email){
-        try{
-        Document doc = database.getCollection("Users").find(new Document("email", email)).first();
-        return doc.getString("password");
-        }catch(Exception e){
-            return "Wrong Email";
+            User user = new User();
+            user.setFirstName((String) document.get("firstName"));
+            user.setSecondName((String) document.get("secondName"));
+            user.setEmail((String) document.get("email"));
+            user.setPassword((String) document.get("password"));
+            return user;
+
         }
 
+        return null;
     }
-
-
-    public static void main(String[] args) {
-        // Replace the uri string with your MongoDB deployment's connection string
-        DataBase database = DataBase.getInstance();
-        System.out.println(database.getPassword("ahmd@gmail.com"));
-
+    public void setUser(User user){
+        Document document = new Document();
+        document.append("firstName", user.getFirstName());
+        document.append("secondName", user.getSecondName());
+        document.append("email", user.getEmail());
+        document.append("password", user.getPassword());
+        database.getCollection("Users").insertOne(document);
     }
+    
 }
--- email-Server-backend/src/main/java/com/example/email/Server/logs/SignIn.java
+++ email-Server-backend/src/main/java/com/example/email/Server/logs/SignIn.java
@@ -3,12 +3,8 @@ package com.example.email.Server.logs;
 import com.example.email.Server.DataBase;
 import com.example.email.Server.controller.SingleTonServer;
 import com.example.email.Server.model.User;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import java.io.File;
-import java.io.IOException;
 
 public class SignIn {
     SingleTonServer server;
@@ -20,7 +16,7 @@ public class SignIn {
         //get the correct password from mongodb
         User currentUser = dataBase.getUser( user.getEmail());
         if (currentUser == null){
-            return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(null,HttpStatus.ACCEPTED);
         }
         else if(currentUser.getPassword().equals(user.getPassword())){
             server.loadUser(currentUser);
