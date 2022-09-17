a0c0480490bba998ae993957d83c4577be54e39f
AhmedElemary57 <honor5766@gmail.com>
2022-09-17 17:03:40 +0200

    Emails Modules and component

---
+++ email-Server-frontend/src/app/models/Email.ts
@@ -0,0 +1,16 @@
+export class Email {
+  _d: string;
+  sender: string;
+  receiver: string;
+  subject: string;
+  content: string;
+
+  constructor() {
+    this._d = "";
+    this.sender = "";
+    this.receiver = "";
+    this.subject = "";
+    this.content = "";
+  }
+
+}
