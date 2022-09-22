6c1f2bee4619cfaa495bbf84a6614e625fa5df3c
AhmedElemary57 <honor5766@gmail.com>
2022-09-22 14:17:09 +0200

    remove backend

--- email-Server-backend/src/main/java/com/example/email/Server/CriteriaPattern/Criteria.java
+++
@@ -1,9 +0,0 @@
-package com.example.email.Server.CriteriaPattern;
-
-import com.example.email.Server.model.Email;
-
-import java.util.List;
-
-public interface Criteria {
-    List<Email> meetCriteria(List<Email> emails, String searchBar);
-}
--- email-Server-backend/src/main/java/com/example/email/Server/DataBaseServices/TrashsServices.java
+++
@@ -1,4 +0,0 @@
-package com.example.email.Server.DataBaseServices;
-
-public class TrashsServices {
-}
